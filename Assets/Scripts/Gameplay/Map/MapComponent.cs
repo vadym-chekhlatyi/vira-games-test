77b5c85b0676e3d7fd1768c76356fad7ace1e141
Vadym Chekhlatyi <94396916+vadym-chekhlatyi@users.noreply.github.com>
2023-07-12 14:13:46 +0300

    Map generation fix and blocks falling added

--- Assets/Scripts/Gameplay/Map/MapComponent.cs
+++ Assets/Scripts/Gameplay/Map/MapComponent.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 
 public class MapComponent : MonoBehaviour
 {
-    void OnBecameInvisible()
-    {
-        GenerateNewPosition();
+    [SerializeField] private float speed = 5;
+    private const string PLAYER_TAG = "Player";
+
+    private void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.CompareTag(PLAYER_TAG)){
+            if(!PlayerController.Instance.IsPaused){
+                StartCoroutine(FallDown());
+            }
+        }
     }
 
-    private void GenerateNewPosition()
+    void OnBecameInvisible()
     {
-        throw new NotImplementedException();
+        gameObject.SetActive(false);
+        MapController.Instance.GenerateNewTile();
+    }
+
+    private IEnumerator FallDown(){
+        while(gameObject.activeInHierarchy){
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
--- Assets/Scripts/Gameplay/Map/MapController.cs
+++ Assets/Scripts/Gameplay/Map/MapController.cs
@@ -10,10 +10,13 @@ public class MapController : MonoBehaviour
 
     [SerializeField] private int objectsToPoolCount;
 
+    [SerializeField] private int maxBlocksFromCenter;
+    private int currentBlockPosition;
+
     private List<GameObject> mapBlockPool;
     private bool isGenerated;
 
-    private Vector3 mapFirstBlockPosition = new Vector3(-2f, -0.5f, 3f);
+    private Vector3 mapFirstBlockPosition = new Vector3(-2.5f, -0.5f, 3.5f);
 
     private void Awake()
     {
@@ -60,23 +63,40 @@ public class MapController : MonoBehaviour
         return null; 
     }
 
+    public void GenerateNewTile(){
+
+    }
+
     private void GenerateMap()
     {
         GameObject previousMapBlock = GetPooledObject();
         previousMapBlock.transform.localPosition = mapFirstBlockPosition;
+        currentBlockPosition = 1;
         previousMapBlock.SetActive(true);
 
         for (int i = 1; i < objectsToPoolCount; i++)
         {
             GameObject mapBlock = GetPooledObject();
 
-            if(GetRandomDirection() == 0)
-            {
-                mapBlock.transform.localPosition = previousMapBlock.transform.localPosition + Vector3.forward;
+            if(currentBlockPosition == maxBlocksFromCenter){
+                SetBlockPosition(mapBlock, previousMapBlock.transform.localPosition, false);
+                currentBlockPosition--;
             }
-            else
-            {
-                mapBlock.transform.localPosition = previousMapBlock.transform.localPosition + Vector3.left;
+            else if(currentBlockPosition == -maxBlocksFromCenter){
+                SetBlockPosition(mapBlock, previousMapBlock.transform.localPosition, true);
+                currentBlockPosition++;
+            }
+            else{
+                if(GetRandomDirection() == 0)
+                {
+                    SetBlockPosition(mapBlock, previousMapBlock.transform.localPosition, true);
+                    currentBlockPosition++;
+                }
+                else
+                {
+                    SetBlockPosition(mapBlock, previousMapBlock.transform.localPosition, false);
+                    currentBlockPosition--;
+                }
             }
 
             mapBlock.SetActive(true);
@@ -85,6 +105,19 @@ public class MapController : MonoBehaviour
         }
 
         isGenerated = true;
+    }
+
+    private void SetBlockPosition(GameObject mapBlock, Vector3 previousBlockPosition, bool isRight){
+        if(isRight){
+            mapBlock.transform.localPosition = previousBlockPosition + Vector3.forward;
+        }
+        else{
+            mapBlock.transform.localPosition = previousBlockPosition + Vector3.left;
+        }
+    }
+    
+    private void SetBlockPositionToRight(GameObject mapBlock){
+
     }
 
     private int GetRandomDirection()
