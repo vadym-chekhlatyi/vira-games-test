d9d0ad3cc3efe8e9e0872e5e74526cd63ca4006a
Vadym Chekhlatyi <94396916+vadym-chekhlatyi@users.noreply.github.com>
2023-07-18 06:40:49 +0300

    GameOverPopup added

--- Assets/Scripts/Popups/GameOverPopup.cs
+++ Assets/Scripts/Popups/GameOverPopup.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour
+public class GameOverPopup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI bestScoreText;
+
+    public void Start(){
+        scoreText.text = ScoresController.Instance.Scores.ToString();
+        bestScoreText.text = PlayerController.Instance.Stats.HighestScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void RestartGame(){
+        SceneManager.LoadScene(0);
     }
 }
